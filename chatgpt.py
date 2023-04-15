3855532607fefe85239f4068adcad88d68dfd59d
asksaito <es.engineer79@gmail.com>
2023-04-15 15:00:16 +0900

    Add settings file

--- chatgpt.py
+++ chatgpt.py
@@ -1,5 +1,6 @@
 import os
 import openai
+import settings
 
 # openai.organization = "org-xxxx"
 openai.api_key = os.getenv("OPENAI_API_KEY")
@@ -7,9 +8,9 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 def call_chatgpt(prompt):
     # ChatGPT completions API https://platform.openai.com/docs/api-reference/completions/create
     response = openai.Completion.create(
-        model = "text-davinci-003",
+        model = settings.CHATGPT_MODEL,
         prompt = prompt,
-        max_tokens = 50
+        max_tokens = settings.CHATGPT_MAX_TOKENS
     )
 
     response_text = response['choices'][0]['text']
---
+++ settings.py
@@ -0,0 +1,3 @@
+#ChatGPT settings
+CHATGPT_MODEL = "text-davinci-003"
+CHATGPT_MAX_TOKENS = 50
